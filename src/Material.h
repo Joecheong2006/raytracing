a838d5ce3d617622afdb54699bd9fe6c4b7e960a
LVOrac <joecheong2006@gmail.com>
2025-03-19 23:43:13 -0700

    add Material.h

---
+++ src/Material.h
@@ -0,0 +1,14 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "glm/glm.hpp"
+
+struct Material {
+    alignas(16) glm::vec3 albedo;
+    float roughness;
+
+    alignas(16) glm::vec3 emissionColor;
+    float emissionRate;
+};
+
+#endif
